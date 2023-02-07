0700c9ccf7c4f3b24127ad54fe2c27dddc14dffe
lby132 <quddbscjswow@naver.com>
2023-02-07 23:26:52 +0900

    react

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import axios from "../api/axios";
 import "./Row.css";
+import MovieModal from "./MovieModal";
 
 export default function Row({isLargeRow, title, id, fetchUrl}) {
     const [movie, setMovies] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [movieSelected, setMovieSelected] = useState({});
 
     useEffect(() => {
         fetchMovieData();
@@ -13,6 +16,11 @@ export default function Row({isLargeRow, title, id, fetchUrl}) {
         const request = await axios.get(fetchUrl);
         console.log('request', request);
         setMovies(request.data.results);
+    }
+
+    const handleClick = (movie) => {
+        setModalOpen(true);
+        setMovieSelected(movie);
     }
 
     return (
@@ -36,6 +44,7 @@ export default function Row({isLargeRow, title, id, fetchUrl}) {
                             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                             src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path: movie.backdrop_path} `}
                             alt={movie.name}
+                            onClick={() => handleClick(movie)}
                         />
                     ))}
                 </div>
@@ -50,6 +59,11 @@ export default function Row({isLargeRow, title, id, fetchUrl}) {
                     </span>
                 </div>
             </div>
+            {
+                modalOpen && (
+                    <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+                )
+            }
         </section>
     );
 }
---
+++ src/pages/SearchPage/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Banner from "../../components/Banner";
+import Row from "../../components/Row";
+import requests from "../../api/requests";
+import Footer from "../../components/Footer";
+
+export default function SearchPage() {
+    return <div></div>;
+}
